54d6d826cef161485c6bfb83314f1057a8945a2d
Ryoma Jodoi <ryoma.jodoi@linecorp.com>
2019-07-23 13:02:51 +0900

    Add LINE Bot Base

---
+++ src/main/java/com/example/linebot/LineBotApplication.java
@@ -0,0 +1,37 @@
+package com.example.linebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import lombok.RequiredArgsConstructor;
+
+@LineMessageHandler
+@SpringBootApplication
+@RequiredArgsConstructor
+public class LineBotApplication {
+
+    private final LineMessagingClient client;
+
+    public static void main(String[] args) {
+        SpringApplication.run(LineBotApplication.class, args);
+    }
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        return TextMessage.builder().text(event.getMessage().getText()).build();
+    }
+
+    @EventMapping
+    public void handleTextMessageEventUsingClient(MessageEvent<TextMessageContent> event) {
+        client.replyMessage(new ReplyMessage(event.getReplyToken(), TextMessage.builder().text(event.getMessage().getText()).build()));
+    }
+
+}
